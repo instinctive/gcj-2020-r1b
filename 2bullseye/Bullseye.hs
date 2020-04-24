5b905c85b91764fbbb37be3d2b48b30c8e545879
Charles Martin <charles.martin@gmail.com>
2020-04-24 12:07:19 -0700

    Use Either type for judge answer

--- 2bullseye/Bullseye.hs
+++ 2bullseye/Bullseye.hs
@@ -2,22 +2,24 @@
 -- https://codingcompetitions.withgoogle.com/codejam/round/000000000019fef2/00000000002d5b63
 -- vim: foldmethod=marker
 
--- pragmas, imports, and utilities 
-{-# LANGUAGE FlexibleContexts     #-}
-{-# LANGUAGE FlexibleInstances    #-}
-{-# LANGUAGE LambdaCase           #-}
-{-# LANGUAGE TupleSections        #-}
+-- pragmas, imports, and utilities
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE PatternSynonyms   #-}
+{-# LANGUAGE TupleSections     #-}
 
 module Main where
 
-import Control.Monad              ( replicateM_, mzero, void )
-import Control.Monad.Trans.Class  ( lift                     )
-import Control.Monad.Trans.Maybe  ( MaybeT, runMaybeT        )
-import Control.Monad.Trans.Reader ( ReaderT, runReaderT, ask )
-import Debug.Trace                ( traceShowM               )
-import System.IO                  ( hFlush, stdout           )
-import Text.Printf                ( printf                   )
-import Text.Read                  ( readMaybe                )
+import Control.Monad              ( replicateM_, join, void     )
+import Control.Monad.Trans.Class  ( lift                        )
+import Control.Monad.Trans.Except ( ExceptT, runExceptT, except )
+import Control.Monad.Trans.Maybe  ( MaybeT, runMaybeT           )
+import Control.Monad.Trans.Reader ( ReaderT, runReaderT, ask    )
+import Debug.Trace                ( traceShowM                  )
+import System.IO                  ( hFlush, stdout              )
+import Text.Printf                ( printf                      )
+import Text.Read                  ( readMaybe                   )
 
 ifM :: Monad m => m Bool -> m a -> m a -> m a
 ifM bm tm em = bm >>= \b -> if b then tm else em
@@ -33,40 +35,48 @@ findMinM test = go where
 getReadList :: Read a => IO [a]
 getReadList = map read . words <$> getLine
 
--- types 
-type Pt = (Int,Int)
+-- types
+type Pt        = (Int,Int)
 
-data Ans = WRONG | MISS | HIT | CENTER deriving (Eq,Read,Show)
+type Answer    = Either Bool Bool
+pattern WRONG  = Left False
+pattern CENTER = Left True
+pattern MISS   = Right False
+pattern HIT    = Right True
 
-class Monad m => MonadIntf m where
-    getInit :: m (Int,Int,Int)
-    isHit   :: Pt -> m Ans
+parse :: String -> Answer
+parse = \case
+    "WRONG"  -> WRONG
+    "CENTER" -> CENTER
+    "MISS"   -> MISS
+    "HIT"    -> HIT
+    s -> error $ printf "invalid answer: %s" (show s)
 
-instance MonadIntf IO where 
-    getInit = do
-        [t,a,b] <- getReadList :: IO [Int]
-        pure (t,a,b)
-    isHit (x,y) = do
+class Monad m => AnswerM m where
+    answer :: Pt -> m Answer
+
+instance AnswerM IO where
+    answer (x,y) = do
         printf "%d %d\n" x y 
         hFlush stdout
-        raw <- getLine
-        maybe (err raw) pure $ readMaybe raw
-      where err = error . printf "bad answer: %s"
+        parse <$> getLine
 
--- testing 
+-- testing
 test :: Monad m => Int -> (Int,Int) -> m ()
 test r ctr = runReaderT go env where
-    go = void $ runMaybeT $ solve (2*r)
+    go = do
+        final <- runExceptT $ solve (2*r)
+        traceShowM final
+        pure ()
     env = Target (r,ctr)
 
 newtype Target = Target (Int,(Int,Int)) deriving Show
 
-instance Monad m => MonadIntf (ReaderT Target m) where 
-    getInit = pure (1,0,0)
-    isHit (x,y) = do
+instance Monad m => AnswerM (ReaderT Target m) where 
+    answer (x,y) = do
         Target (r, (x0,y0)) <- ask
         let ans = check (x-x0) (y-y0) r
-        -- traceShowM ((x,y),ans)
+        traceShowM ((x,y),ans)
         pure ans
       where
         check 0 0 _ = CENTER
@@ -74,41 +84,37 @@ instance Monad m => MonadIntf (ReaderT Target m) where
             | u*u + v*v > r*r = MISS
             | otherwise       = HIT
 
--- solution 
+-- main
 kSize = 10^9 :: Int -- size is constant in codejam problem
 
 main :: IO ()
-main = codeJam kSize
+main = do
+    (t:_) <- getReadList :: IO [Int]
+    replicateM_ t $ runExceptT $ solve kSize
 
-codeJam :: MonadIntf m => Int -> m ()
-codeJam size = do
-    (t,_,_) <- getInit
-    replicateM_ t $ runMaybeT $ solve size
+-- solution
+type Outer = ExceptT Bool
 
-isHit' :: MonadIntf m => Pt -> MaybeT m Bool
-isHit' pt = lift (isHit pt) >>= \case
-    WRONG  -> error "got WRONG answer"
-    CENTER -> mzero -- bullseye so now we break from MaybeT
-    ans -> lift . pure $ ans == HIT
+isHit :: AnswerM m => Pt -> Outer m Bool
+isHit pt = lift (answer pt) >>= except
 
-findTarget :: MonadIntf m => Int -> MaybeT m (Maybe Pt)
+findTarget :: AnswerM m => Int -> Outer m (Maybe Pt)
 findTarget size = go $ (0,0) : quads
   where
     quads = (,) <$> [-h,h] <*> [-h,h] where h = div size 2
     go [] = pure Nothing
     go (pt:more) =
-        ifM (isHit' pt) (pure $ Just pt) (go more)
+        ifM (isHit pt) (pure $ Just pt) (go more)
 
-solve :: MonadIntf m => Int -> MaybeT m ()
+solve :: AnswerM m => Int -> Outer m ()
 solve size = findTarget size >>= \case
     Nothing -> error "could not find target"
     Just (x,y) -> do
-        xlo <- findMinM (isHit' . (,y)) (-size) x
-        ylo <- findMinM (isHit' . (x,)) (-size) y
-        xhi <- negate <$> findMinM (isHit' . (,y) . negate) (-size) (-x)
-        yhi <- negate <$> findMinM (isHit' . (x,) . negate) (-size) (-y)
+        xlo <- findMinM (isHit . (,y)) (-size) x
+        ylo <- findMinM (isHit . (x,)) (-size) y
+        xhi <- negate <$> findMinM (isHit . (,y) . negate) (-size) (-x)
+        yhi <- negate <$> findMinM (isHit . (x,) . negate) (-size) (-y)
         let xctr = div (xlo + xhi) 2
         let yctr = div (ylo + yhi) 2
-        -- traceShowM ((xlo,xhi),(ylo,yhi))
-        _ <- isHit' (xctr,yctr) -- should exit MaybeT on CENTER result
+        void $ isHit (xctr,yctr) -- should break from Outer on CENTER result
         error "missed on bullseye hit"
