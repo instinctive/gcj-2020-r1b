13442a078a58084175862781e5ad44a8122a5d70
Charles Martin <charles.martin@gmail.com>
2020-04-24 12:53:25 -0700

    Move EitherT into the Answer type

--- 2bullseye/Bullseye.hs
+++ 2bullseye/Bullseye.hs
@@ -2,7 +2,8 @@
 -- https://codingcompetitions.withgoogle.com/codejam/round/000000000019fef2/00000000002d5b63
 -- vim: foldmethod=marker
 
--- pragmas, imports, and utilities
+-- pragmas, imports, and utilities {{{1
+
 {-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase        #-}
@@ -11,15 +12,13 @@
 
 module Main where
 
-import Control.Monad              ( replicateM_, join, void     )
+import Control.Monad              ( replicateM_, void           )
 import Control.Monad.Trans.Class  ( lift                        )
 import Control.Monad.Trans.Except ( ExceptT, runExceptT, except )
-import Control.Monad.Trans.Maybe  ( MaybeT, runMaybeT           )
 import Control.Monad.Trans.Reader ( ReaderT, runReaderT, ask    )
 import Debug.Trace                ( traceShowM                  )
 import System.IO                  ( hFlush, stdout              )
 import Text.Printf                ( printf                      )
-import Text.Read                  ( readMaybe                   )
 
 ifM :: Monad m => m Bool -> m a -> m a -> m a
 ifM bm tm em = bm >>= \b -> if b then tm else em
@@ -35,33 +34,55 @@ findMinM test = go where
 getReadList :: Read a => IO [a]
 getReadList = map read . words <$> getLine
 
--- types
+-- types {{{1
+
 type Pt        = (Int,Int)
 
+-- If the judge responds CENTER or WRONG, then we are done,
+-- and the Left result will break out of our monad.
+-- The Right result is True iff the query point was a hit.
 type Answer    = Either Bool Bool
-pattern WRONG  = Left False
+
 pattern CENTER = Left True
-pattern MISS   = Right False
+pattern WRONG  = Left False
 pattern HIT    = Right True
+pattern MISS   = Right False
 
 parse :: String -> Answer
 parse = \case
-    "WRONG"  -> WRONG
     "CENTER" -> CENTER
-    "MISS"   -> MISS
+    "WRONG"  -> WRONG
     "HIT"    -> HIT
+    "MISS"   -> MISS
     s -> error $ printf "invalid answer: %s" (show s)
 
+-- The returned Bool is True iff we hit the CENTER,
+-- and False iff we received WRONG.
+type Return = ExceptT Bool
+
 class Monad m => AnswerM m where
-    answer :: Pt -> m Answer
+    isHit :: Pt -> Return m Bool
+
+-- main {{{1
+
+kSize = 10^9 :: Int -- size is constant in codejam problem
+
+main :: IO ()
+main = do
+    (t:_) <- getReadList :: IO [Int]
+    replicateM_ t $ runExceptT $ solve kSize
 
 instance AnswerM IO where
-    answer (x,y) = do
-        printf "%d %d\n" x y 
-        hFlush stdout
-        parse <$> getLine
+    isHit pt = lift (query pt) >>= except
 
--- testing
+query :: Pt -> IO Answer
+query (x,y) = do
+    printf "%d %d\n" x y
+    hFlush stdout
+    parse <$> getLine
+
+-- test {{{1
+
 test :: Monad m => Int -> (Int,Int) -> m ()
 test r ctr = runReaderT go env where
     go = do
@@ -73,40 +94,20 @@ test r ctr = runReaderT go env where
 newtype Target = Target (Int,(Int,Int)) deriving Show
 
 instance Monad m => AnswerM (ReaderT Target m) where 
-    answer (x,y) = do
-        Target (r, (x0,y0)) <- ask
+    isHit (x,y) = do
+        Target (r, (x0,y0)) <- lift ask
         let ans = check (x-x0) (y-y0) r
         traceShowM ((x,y),ans)
-        pure ans
+        except ans
       where
         check 0 0 _ = CENTER
         check u v r 
             | u*u + v*v > r*r = MISS
             | otherwise       = HIT
 
--- main
-kSize = 10^9 :: Int -- size is constant in codejam problem
+-- solve {{{1
 
-main :: IO ()
-main = do
-    (t:_) <- getReadList :: IO [Int]
-    replicateM_ t $ runExceptT $ solve kSize
-
--- solution
-type Outer = ExceptT Bool
-
-isHit :: AnswerM m => Pt -> Outer m Bool
-isHit pt = lift (answer pt) >>= except
-
-findTarget :: AnswerM m => Int -> Outer m (Maybe Pt)
-findTarget size = go $ (0,0) : quads
-  where
-    quads = (,) <$> [-h,h] <*> [-h,h] where h = div size 2
-    go [] = pure Nothing
-    go (pt:more) =
-        ifM (isHit pt) (pure $ Just pt) (go more)
-
-solve :: AnswerM m => Int -> Outer m ()
+solve :: AnswerM m => Int -> Return m ()
 solve size = findTarget size >>= \case
     Nothing -> error "could not find target"
     Just (x,y) -> do
@@ -118,3 +119,11 @@ solve size = findTarget size >>= \case
         let yctr = div (ylo + yhi) 2
         void $ isHit (xctr,yctr) -- should break from Outer on CENTER result
         error "missed on bullseye hit"
+
+findTarget :: AnswerM m => Int -> Return m (Maybe Pt)
+findTarget size = go $ (0,0) : quads
+  where
+    quads = (,) <$> [-h,h] <*> [-h,h] where h = div size 2
+    go [] = pure Nothing
+    go (pt:more) =
+        ifM (isHit pt) (pure $ Just pt) (go more)
